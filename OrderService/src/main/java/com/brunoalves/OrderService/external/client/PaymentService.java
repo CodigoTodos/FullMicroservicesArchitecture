dff4fb8a08b4f75aa033f39d1e42d39727dd90a8
Bruno Alves <bmalves@criticalsoftware.com>
2022-11-09 09:18:02 +0000

    Updated OrderService and CloudGateway

--- OrderService/src/main/java/com/brunoalves/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/brunoalves/OrderService/external/client/PaymentService.java
@@ -2,11 +2,14 @@ package com.brunoalves.OrderService.external.client;
 
 import com.brunoalves.OrderService.exception.CustomException;
 import com.brunoalves.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
--- OrderService/src/main/java/com/brunoalves/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/brunoalves/OrderService/external/client/ProductService.java
@@ -1,12 +1,16 @@
 package com.brunoalves.OrderService.external.client;
 
 
+import com.brunoalves.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -15,5 +19,11 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default ResponseEntity<Void> fallback(Exception e) {
+        throw new CustomException("Product Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 
 }
